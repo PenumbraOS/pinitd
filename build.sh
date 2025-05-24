84849778aec7a2ec9006930a1d3614fd21e1a91b
Adam Gastineau <adam@agg.im>
2025-05-24 06:19:47 -0700

    Automatically push new APK to device when building

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ adb push target/aarch64-linux-android/release/pinitd-cli /data/local/tmp/bin/pin
 # adb shell chmod +x /data/local/tmp/bin/pinitd
 adb shell chmod +x /data/local/tmp/bin/pinitd-cli
 cp target/aarch64-linux-android/release/pinitd android/app/src/main/jniLibs/arm64-v8a/libpinitd.so
+cd android && ./gradlew installDebug
